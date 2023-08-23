6af791c1b19a7f85d7a3bcfbfd3409dab72b3376
Raul Diego <rauldiegnav@gmail.com>
2023-08-23 17:42:34 +0200

    SQL 50 - ejercicios

---
+++ Ingeniería de datos/Ejercicios Entrevistas/SQL/SQL 50.sql
@@ -0,0 +1,38 @@
+
+-- Find the ids of products that are both low fat and recyclable
+
+SELECT product_id
+FROM Products
+WHERE low_fats = 'Y' AND recyclable='Y';
+
+
+-- Find the names of the customer that are not referred by the customer with id = 2
+
+SELECT name
+FROM Customer
+WHERE referee_id <> 2 OR referee_id is NULL;
+
+-- Write a solution to find the name, population, and area of the big countries.
+
+SELECT name, population, area
+FROM World
+WHERE population >= 25000000  OR area >= 3000000 ;
+
+-- Find all the authors that viewed at least one of their own articles
+
+SELECT DISTINCT author_id id
+FROM Views
+WHERE author_id = viewer_id
+ORDER BY author_id;
+
+
+-- find the IDs of the invalid tweets. 
+-- The tweet is invalid if the number of characters used in the content 
+-- of the tweet is strictly greater than 15
+
+SELECT DISTINCT tweet_id
+FROM Tweets
+WHERE LENGTH(content) > 15;
+
+-- Write a solution to show the unique ID of each user, 
+-- If a user does not have a unique ID replace just show null
