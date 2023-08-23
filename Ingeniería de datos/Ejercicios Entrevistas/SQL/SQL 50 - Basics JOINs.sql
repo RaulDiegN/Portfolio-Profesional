27facc765a4f864554d3f75f400c9623e22aab2a
Raul Diego <rauldiegnav@gmail.com>
2023-08-23 18:10:02 +0200

    JOINS SQL 50

--- Ingeniería de datos/Ejercicios Entrevistas/SQL/SQL 50 - Basics JOINs.sql
+++ Ingeniería de datos/Ejercicios Entrevistas/SQL/SQL 50 - Basics JOINs.sql
@@ -35,3 +35,12 @@ LEFT JOIN (
 ) b ON a.recordDate = b.yesterday
 WHERE a.temperature > b.temperature;
 
+-- Write a solution to find the average time each machine takes to complete a process.
+
+SELECT a.machine_id, ROUND(AVG(b.timestamp - a.timestamp),3) processing_time
+FROM Activity a
+LEFT JOIN Activity b
+ON a.machine_id = b.machine_id AND a.process_id = b.process_id
+WHERE a.activity_type = 'start' AND b.activity_type='end'
+GROUP BY a.machine_id;
+
