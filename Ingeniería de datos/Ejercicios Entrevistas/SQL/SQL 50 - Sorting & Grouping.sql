ef3f663d6aacde3b192ab60fa4e969312b547724
Raul Diego <71923972+RaulDiegN@users.noreply.github.com>
2023-09-11 18:33:22 +0200

    Nuevos ejercicios

--- Ingeniería de datos/Ejercicios Entrevistas/SQL/SQL 50 - Sorting & Grouping.sql
+++ Ingeniería de datos/Ejercicios Entrevistas/SQL/SQL 50 - Sorting & Grouping.sql
@@ -1,8 +1,24 @@
 -- Write a solution to calculate the number of unique subjects each teacher teaches in the university.
 
-SELECT *
-FROM Cinema
-WHERE MOD(id,2) <> 0 and description <> 'boring'
-ORDER BY rating DESC;
+SELECT DISTINCT teacher_id, COUNT(DISTINCT subject_id) AS cnt
+FROM Teacher
+GROUP BY teacher_id
 
 
+-- Write a solution to find the daily active user count for a period of 30 days ending 2019-07-27 inclusively.
+-- A user was active on someday if they made at least one activity on that day
+
+SELECT activity_date as day, COUNT( DISTINCT user_id) AS active_users
+FROM Activity
+WHERE datediff('2019-07-27', activity_date) < 30 AND activity_date <= '2019-07-27'
+GROUP BY activity_date
+
+-- Write a solution to select the product id, year, quantity, and price for the first year of every product sold.
+
+SELECT DISTINCT product_id, year AS first_year, quantity, price
+FROM Sales
+WHERE (product_id, year) IN (
+  SELECT product_id, MIN(year)
+  FROM Sales
+  GROUP BY product_id
+)
