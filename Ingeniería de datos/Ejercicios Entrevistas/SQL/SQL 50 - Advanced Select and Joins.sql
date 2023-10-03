958da4dc7e8a16bbf9cbcea1b5c1e8dccd35f71b
Raul Diego <71923972+RaulDiegN@users.noreply.github.com>
2023-10-03 08:58:08 +0200

    Nuevos Ejercicios

--- Ingeniería de datos/Ejercicios Entrevistas/SQL/SQL 50 - Advanced Select and Joins.sql
+++ Ingeniería de datos/Ejercicios Entrevistas/SQL/SQL 50 - Advanced Select and Joins.sql
@@ -27,3 +27,29 @@ SELECT x, y, z,
   CASE WHEN (x+y>z) AND (x+z>y) AND (y+z>x) THEN 'Yes'
   ELSE 'No' END triangle
 FROM Triangle;
+
+
+-- Find all numbers that appear at least three times consecutively.
+
+SELECT DISTINCT a.num ConsecutiveNums
+FROM Logs a
+INNER JOIN Logs b ON a.id = b.id+1
+INNER JOIN Logs c ON a.id = c.id+2
+WHERE a.num = b.num and b.num = c.num;
+
+
+-- Write a solution to find the prices of all products on 2019-08-16. 
+-- Assume the price of all products before any change is 10.
+
+WITH cte AS
+(SELECT *, RANK() OVER (PARTITION BY product_id ORDER BY change_date DESC) AS r 
+FROM Products
+WHERE change_date<= '2019-08-16')
+
+SELECT product_id, new_price AS price
+FROM cte
+WHERE r = 1
+UNION
+SELECT product_id, 10 AS price
+FROM Products
+WHERE product_id NOT IN (SELECT product_id FROM cte);
